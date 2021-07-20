59db22fc6c7b1d801fb1230aa20eb518f48e9ea6
ninjaflautist90 <aleena.polansky@gmail.com>
2021-07-20 13:52:49 +0000

    next iteration

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function compute()
 {
+    alert("error")
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
